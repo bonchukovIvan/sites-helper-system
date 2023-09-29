9d86486f199d5cfcc1ba7d8e33c82d9d18d7233e
ivanbonchukov <i.bonchukov@linet.sumdu.edu.ua>
2023-09-29 16:21:08 +0300

    add google api

--- index.php
+++ index.php
@@ -4,11 +4,5 @@ use app\Application;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-
-@$domains_list_url = $argv[1] ? $argv[1] : null;
-@$slack_token = $argv[2] ? $argv[2] : null;
-@$slack_channel = $argv[3] ? $argv[3] : null;
-@$mail_to = $argv[4] ? $argv[4] : null;
-
 $app = new Application();
-$app->start($domains_list_url, $slack_token, $slack_channel, $mail_to);
+$app->start();
--- src/Application.php
+++ src/Application.php
@@ -2,52 +2,63 @@
 
 namespace app;
 
-use app\web\CsvParser;
+use app\helpers\ConfigHelper;
 use app\web\DomainsChecker;
 
-use app\log\Logger;
-use app\messangers\Mailer;
 use app\helpers\ArrayHelper;
+use app\helpers\SheetHelper;
+
+use app\log\Logger;
+
+use app\messangers\Mailer;
 use app\messangers\Slack;
 
+
 class Application {
+
     private function init(): bool {
         date_default_timezone_set('Etc/GMT-3');
         if (ini_get('max_execution_time') >= 30) {
             ini_set('max_execution_time', 600);
         }
+
         return true;
     }
 
-    public function message($name) {
-        return 'Hi, '. $name;
-    }
-
-    public function start($domains_list_url = null, $token = null, $channel = null, $to = null) {
+    public function start() {
         self::init();
+        $config = new ConfigHelper();
 
-        if (!$domains_list_url) {
-            Logger::get_last_log();
-            return true;
-        }
+        $token = $config->get_config()['SLACK_TOKEN'];
+        $channel = $config->get_config()['SLACK_CHANNEL'];
+        $to = $config->get_config()['MAIL_TO'];
 
         $logger = new Logger();
         $logger->init();
 
-        $domains_array = CsvParser::get_domains_from_csv($domains_list_url);
+        $sheet_helper = new SheetHelper();
+        $domains_array = $sheet_helper->get_domains();
         $checked_domains_list = DomainsChecker::create_domain_list($domains_array);
         arsort($checked_domains_list);
 
         $suspected_domains = ArrayHelper::create_suspected_array($checked_domains_list);
 
+        if (!empty($suspected_domains)) {
+            $updated_domains = ArrayHelper::create_updated_array($checked_domains_list);
+            
+            $sheet_helper->clear_domains(count($domains_array));
+            $sheet_helper->update_domains(array_keys($updated_domains));
+            $sheet_helper->update_suspected_domains(array_keys($suspected_domains));
+        }
+
         $logger->write_domains_to_log($checked_domains_list);
 
-        if ($token && $channel) {
+        if ($token != '' && $channel != '') {
             $slack = new Slack();
             $slack->send_to_slack($suspected_domains, $token, $channel);
         }
 
-        if ($to) {
+        if ($to != '') {
             $mailer = new Mailer();
             $mailer->send_to_mail($suspected_domains, $to);
         }
